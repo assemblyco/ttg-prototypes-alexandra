97079f175c885ff871a98d2c5df1233f8c798073
Alexandra Nemes <alexandra@assemblyco.com>
2017-07-10 11:23:12 -0700

    Additional data uploaded

---
+++ Dawgs.js
@@ -0,0 +1,8 @@
+export let Dawgs=[
+  {key: "https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi"},
+  {key: "https://visitmontgomery.com/wp-content/uploads/2014/08/happy-dog.jpg"},
+  {key: "https://www.omlet.co.uk/images/originals/Dog-Dog_Guide-A_Golden_Retriever_with_a_lovely_thick_coat.jpg"},
+  {key: "https://do8w60hxkwzup.cloudfront.net/wp-content/uploads/2015/01/Adopt-A-Puppy.jpg"},
+  {key: "https://www.happyhounddogcare.ca/images/2016HappyHoundphotos/dogs25-18.jpg"}
+    
+]
---
+++ catz.js
@@ -0,0 +1,8 @@
+export let Catz=[
+  {key: "https://www.petmd.com/sites/default/files/hypoallergenic-cat-breeds.jpg"},
+  {key: "https://meaningfilled.com/wp-content/uploads/2016/02/cat-praying.jpg"},
+  {key: "https://pets3000.com/images/cats/cats-89.jpg"},
+  {key: "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg"},
+  {key: "https://www.rd.com/wp-content/uploads/sites/2/2016/04/06-cat-wants-to-tell-you-gift.jpg"}
+    
+]
