a93e7a1393ef3069fe27f8575a0a8668a91f94c7
Alexandra Nemes <alexandra@assemblyco.com>
2017-07-21 11:04:44 -0700

    Refactored 'Default behavior...'

--- graph.js
+++ graph.js
@@ -8,34 +8,34 @@ import {
   View
 } from 'react-native';
 
-import { generalStyles } from  './generalStyles';
-
 const {
-  Surface,
   Group,
   Shape,
+  Surface,
 } = ART;
 
 export class Graph extends Component {
   constructor(props){
-    super(props)
-    this.props.path= "M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+    super()
+    
+    this.state={
+      linePath: "M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+    }
   }
   render() {
-    console.log(ART);
     return (
-      <View style={generalStyles.container}>
-        <Surface width={200} height={200}>
-          <Group x={0} y={0}>
-            <Shape
-              d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
-              //d={ this.props.path }
-              stroke="#000"
-              strokeWidth={1}
-            />
-          </Group>
-        </Surface>
-      </View>
+      //<Source /> is the equivalent of <svg />
+      <Surface width={200} height={200}>
+        {/*<Group /> is the equivalent of <g />*/}
+        <Group x={0} y={0}>
+          {/*<Shape /> is the equivalent of <path />*/}
+          <Shape
+            d={ this.state.linePath }
+            stroke="#000"
+            strokeWidth={1}
+          />
+        </Group>
+      </Surface>
     );
   }
 }
