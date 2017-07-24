1fd1cd9755faf6682ce4c60524cb4584235f5e00
Alexandra Nemes <alexandra@assemblyco.com>
2017-07-24 16:02:37 -0700

    react-native-push-notification

--- android/app/src/main/java/com/infinitescroll/MainActivity.java
+++ android/app/src/main/java/com/infinitescroll/MainActivity.java
@@ -1,15 +1,32 @@
 package com.infinitescroll;
 
 import com.facebook.react.ReactActivity;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+  /**
+    * Returns the name of the main component registered from JavaScript.
+    * This is used to schedule rendering of the component.
+    */
+  @Override
+  protected String getMainComponentName() {
+    return "InfiniteScroll";
+  }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected String getMainComponentName() {
-        return "InfiniteScroll";
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new ReactNativePushNotificationPackage()
+      );
+    }
+  };
 }
