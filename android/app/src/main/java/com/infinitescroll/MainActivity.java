303c6deee7ab632a0fcf3cbb31632a2032e0aec3
Alexandra Nemes <alexandra@assemblyco.com>
2017-07-25 15:19:59 -0700

    Fixed bug related to react-native-push-notification plugin

--- android/app/src/main/java/com/infinitescroll/MainActivity.java
+++ android/app/src/main/java/com/infinitescroll/MainActivity.java
@@ -1,32 +1,15 @@
 package com.infinitescroll;
 
 import com.facebook.react.ReactActivity;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-    * Returns the name of the main component registered from JavaScript.
-    * This is used to schedule rendering of the component.
-    */
-  @Override
-  protected String getMainComponentName() {
-    return "InfiniteScroll";
-  }
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    protected String getMainComponentName() {
+        return "InfiniteScroll";
     }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new ReactNativePushNotificationPackage()
-      );
-    }
-  };
 }
